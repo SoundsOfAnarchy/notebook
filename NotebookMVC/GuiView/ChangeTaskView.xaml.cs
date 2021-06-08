9ccaa37330cb2faaef1e47ca6a5811dd838a9363
SoundsOfAnarchy <61351538+SoundsOfAnarchy@users.noreply.github.com>
2021-06-08 14:14:20 +0300

    Add files via upload

---
+++ NotebookMVC/GuiView/ChangeTaskView.xaml.cs
@@ -0,0 +1,91 @@
+﻿using NotebookMVC.Controller;
+using NotebookMVC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GuiView
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeTaskView.xaml
+    /// </summary>
+    public partial class ChangeTaskView : Window
+    {
+        public INotebookController Controller { get; set; }
+        private int _id;
+        
+        public ChangeTaskView(INotebookController controller, int id)
+        {
+            InitializeComponent();
+
+            Controller = controller;
+            _id = id;
+
+            for (int i = 0; i < 24; ++i)
+                hourComboBox.Items.Add(i);
+
+            for (int i = 0; i < 60; ++i)
+                minutesComboBox.Items.Add(i);
+
+            completionComboBox.ItemsSource = Enum.GetValues(typeof(TaskCompletion));
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dateTimePicker1.SelectedDate == null)
+            {
+                MessageBox.Show("Date isn't seleceted");
+                return;
+            }
+            if (hourComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Hour isn't seleceted");
+                return;
+            }
+            if (minutesComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Minutes isn't seleceted");
+                return;
+            }
+            if (durationTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Duration isn't enter");
+                return;
+            }
+            if (completionComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Completion isn't selected");
+                return;
+            }
+            if (textTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Text isn't enter");
+                return;
+            }
+
+            DateTime date = (DateTime)dateTimePicker1.SelectedDate;
+            date = date.AddHours(Convert.ToDouble(hourComboBox.SelectedItem));
+            date = date.AddMinutes(Convert.ToDouble(minutesComboBox.SelectedItem));
+            double duration = Convert.ToDouble(durationTextBox.Text);
+            TaskCompletion completion = (TaskCompletion)completionComboBox.SelectedItem;
+            string text = textTextBox.Text;
+            Controller.ChangeTask(_id, date, TimeSpan.FromMinutes(duration), completion, text);
+            Close();
+        }
+    }
+}
---
+++ NotebookMVC/GuiView/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using NotebookMVC.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GuiView
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public INotebookController Controller { get; set; }
+        TimeSpan duration_for_search { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Controller = new NotebookController(@"your_file_path_to_bd");
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            TaskView tv = new TaskView(Controller);
+            tv.ShowDialog();
+        }
+
+        private void dateTimePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(dateTimePicker.SelectedDate != null)
+            {
+                listBox.ItemsSource = Controller.FindTasksByDate((DateTime)dateTimePicker.SelectedDate, TimeSpan.FromHours(24));                
+            }
+        }
+
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem == null) MessageBox.Show("select task on listbox", "error");
+            else
+            {
+                NotebookMVC.Model.Task task = (NotebookMVC.Model.Task)listBox.SelectedItem;
+                ChangeTaskView tv = new ChangeTaskView(Controller, task.ID);
+                tv.dateTimePicker1.SelectedDate = task.DateTime;
+                tv.hourComboBox.SelectedIndex = task.DateTime.Hour;
+                tv.minutesComboBox.SelectedIndex = task.DateTime.Minute;
+                tv.durationTextBox.Text = task.Length.TotalMinutes.ToString();
+                tv.completionComboBox.SelectedIndex = (int)task.Completion;
+                tv.textTextBox.Text = task.TaskText;
+                tv.ShowDialog();
+            }
+        }
+
+        private void removeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem == null) MessageBox.Show("select task on listbox", "error");
+            else
+            {
+                NotebookMVC.Model.Task task = (NotebookMVC.Model.Task)listBox.SelectedItem;
+                Controller.RemoveTask(task.ID);
+            }
+        }
+
+        private void findTasksByDateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void findCancelledTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dateTimePicker.SelectedDate == null) MessageBox.Show("select date on dateTimePicker", "error");
+            else
+            {          
+                TimeDurationWindow tdw = new TimeDurationWindow(this, Controller, NotebookMVC.Model.TaskCompletion.Canceled);
+                tdw.ShowDialog();
+            }
+        }
+
+        private void findTasksByTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextSearchWindow tsw = new TextSearchWindow(this, Controller);
+            tsw.ShowDialog();
+        }
+        private void findInProgressTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dateTimePicker.SelectedDate == null) MessageBox.Show("select date on dateTimePicker", "error");
+            else
+            {
+                TimeDurationWindow tdw = new TimeDurationWindow(this, Controller, NotebookMVC.Model.TaskCompletion.InProgress);
+                tdw.ShowDialog();
+            }
+        }
+
+        private void findCompleteTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dateTimePicker.SelectedDate == null) MessageBox.Show("select date on dateTimePicker", "error");
+            else
+            {
+                TimeDurationWindow tdw = new TimeDurationWindow(this, Controller, NotebookMVC.Model.TaskCompletion.Complete);
+                tdw.ShowDialog();
+            }
+        }
+
+        private void getAllTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.ItemsSource = Controller.GetAllTasks();
+        }
+    }
+}
